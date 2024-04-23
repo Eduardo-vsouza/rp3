f912e0e9129b20ba9a008f3c7fc186bad3df5e6b
Eduardo-vsouza <60533781+Eduardo-vsouza@users.noreply.github.com>
2024-04-23 14:44:41 -0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# from .pipeline_config import Pipeline
-# from .search import MSFragger
-from .post_process import PercolatorPostProcessing
-from .translation_ssh import GTFtoFasta
-from .quantification import MOFF
-from .utils import group_folder_generator
-from .extra_filter import ExtraFilter
-# from .pipeline_config import *
