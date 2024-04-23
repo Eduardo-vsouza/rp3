43701762be5f3d4f356b7dfc37429522f427192b
Eduardo-vsouza <60533781+Eduardo-vsouza@users.noreply.github.com>
2024-04-23 14:44:32 -0700

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .folder_generators import group_folder_generator
-from .check_dirs import check_multiple_dirs
-from .params import Params
-from .blast_filter import BlastParser
-from .gtf_filter import GTFFilter
-from .cat_gtfs import GTFGatherer
--- utils/check_dirs.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def check_multiple_dirs(folders):
-    for folder in folders:
-        if not os.path.exists(folder):
-            os.mkdir(folder)
